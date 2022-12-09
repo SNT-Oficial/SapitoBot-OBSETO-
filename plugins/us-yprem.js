dc61da2f3cc929ebed7568c981d66604a20dd715
Matt Melendez <118824091+Ne-Kosmic@users.noreply.github.com>
2022-12-09 06:57:03 -0500

    ✍️🌌

--- plugins/us-yprem.js
+++ plugins/us-yprem.js
@@ -1,22 +1,26 @@
+/**
+[ By @NeKosmic || https://github.com/NeKosmic/ ]
+**/
 import db from '../lib/database.js'
 
-let handler = async (m, { conn, text, command }) => {
+let handler = async (m, { conn, text, command, args }) => {
     let etiqueta
     if (m.isGroup) etiqueta = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else etiqueta = m.chat
     let user = db.data.users[etiqueta]
     if (!etiqueta) return m.reply(`Por favor etiquete o mencioné a alguien!`)
     let txt = text.replace('@' + etiqueta.split`@`[0], '').trim()
-    if (!txt) return m.reply(`Por favor indique el número de dias`)
-    if (isNaN(txt)) return m.reply(`Solo números!\n\nEjemplo de uso:\n${Prefijo + command} @${m.sender.split`@`[0]} 1`)
-    var nDias = 86400000 * txt
+    if (!args[1]) return m.reply(`Por favor indique el número de dias`)
+    if (isNaN(args[1])) return m.reply(`Solo números!\n\nEjemplo de uso:\n${Prefijo + command} @${m.sender.split`@`[0]} 1`)
+    if (8 <= args[1]) return m.reply(`[ ! ] Maximo 7 dias`)
+    var nDias = 86400000 * args[1]
     var now = new Date() * 1
     if (now < user.premiumTime) user.premiumTime += nDias
     else user.premiumTime = now + nDias
 user.premium = true
     m.reply(`*[ USUARIO PREMIUM ]*
 🔖 *Nombre:* ${user.name}
-📆 *Dias:* ${txt} dia(s)
+📆 *Dias:* ${args[1]} dia(s)
 📉 *Tiempo restante:* ${user.premiumTime - now}MS`)
 }
 handler.help = ['addprem [@usuario] <dias>']
